44295e3f323b61e2bd6457ec875ce1c210df209e
Dan Ibanez <daibane@sandia.gov>
2017-04-05 09:12:58 -0600

    actually install fix_ave_histo_kokkos

--- src/KOKKOS/Install.sh
+++ src/KOKKOS/Install.sh
@@ -79,6 +79,8 @@ action dihedral_opls_kokkos.cpp dihedral_opls.cpp
 action dihedral_opls_kokkos.h dihedral_opls.h
 action domain_kokkos.cpp
 action domain_kokkos.h
+action fix_ave_histo_kokkos.cpp
+action fix_ave_histo_kokkos.h
 action fix_deform_kokkos.cpp
 action fix_deform_kokkos.h
 action fix_eos_table_rx_kokkos.cpp fix_eos_table_rx.cpp
